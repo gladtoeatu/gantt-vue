950a5d23a0490860e949f5da662e006f8d833a48
frank <noikiy@qq.com>
2024-10-11 08:44:40 +0800

    gantt

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <GanttComponent class="left-container" :tasks="tasks"></GanttComponent>
+  </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GanttComponent from './components/GanttComponent.vue';
+ 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  components: {GanttComponent},
+  data () {
+    return {
+      tasks: {
+        data: [
+          {id: 1, text: 'Task #1', start_date: '2020-01-17', duration: 3, progress: 0.6},
+          {id: 2, text: 'Task #2', start_date: '2020-01-20', duration: 3, progress: 0.4},
+          {id: 3, text: 'Task #3', start_date: '2020-01-20', duration: 3, progress: 1}
+        ],
+        links: [
+          {id: 1, source: 1, target: 2, type: '0'}
+        ]
+      },
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  .container {
+    height: 100vh;
+    width: 100%;
+  }
+  .left-container {
+    overflow: hidden;
+    position: relative;
+    height: 100%;
+  }
 </style>
